0f740f2016f5b6460c53b7aaa7a6d3153c810534
Fauxfrenzy82 <rkmwogeza@gmail.com>
2026-07-13 00:45:36 +0300

    Update protocolinkClient.ts

--- src/execution/protocolinkClient.ts
+++ src/execution/protocolinkClient.ts
@@ -1,5 +1,4 @@
 import * as api from '@protocolink/api';
-import * as common from '@protocolink/common';
 import { activeChain } from '../config/chains';
 import { executionWallet } from '../treasury/wallets';
 import { createLogger } from '../utils/logger';
@@ -18,25 +17,4 @@ export function getChainId(): number {
   return activeChain.chainId;
 }
 
-export function getAgentAddress(): Promise<string> {
-  return api.getAgent(activeChain.chainId, executionWallet.address);
-}
-
-export async function getPermit2Status(tokenAddress: string): Promise<boolean> {
-  try {
-    const allowance = await api.protocols.permit2.getAllowance(
-      activeChain.chainId,
-      executionWallet.address,
-      tokenAddress
-    );
-    return allowance !== undefined && allowance !== '0';
-  } catch (err) {
-    log.warn('Failed to check Permit2 allowance', {
-      tokenAddress,
-      error: err instanceof Error ? err.message : String(err),
-    });
-    return false;
-  }
-}
-
-export { api, common };
+// FIX: getAgent may not
