a1c33049b66712a7a7ca80c2c62000973ddfa1d6
Fauxfrenzy82 <rkmwogeza@gmail.com>
2026-08-22 02:00:39 +0300

    Create src/strategies/classicIncentive/buildActionPlan.ts

---
+++ src/strategies/classicIncentive/buildActionPlan.ts
@@ -0,0 +1,5 @@
+import { OpportunityCandidate, ActionPlan } from '../common/opportunityCandidate';
+
+export async function buildActionPlan(candidate: OpportunityCandidate): Promise<ActionPlan> {
+  throw new Error('Classic Incentive not implemented');
+}
