33e3823b9eceeb227ef139b317fe9b733dbe4af0
Fauxfrenzy82 <rkmwogeza@gmail.com>
2026-08-22 23:00:38 +0300

    Update buildActionPlan.ts

--- src/strategies/classicIncentive/buildActionPlan.ts
+++ src/strategies/classicIncentive/buildActionPlan.ts
@@ -1,5 +1,9 @@
 import { OpportunityCandidate, ActionPlan } from '../common/opportunityCandidate';
+import { FlashLoanProvider } from '../../execution/ensoBuilder';
 
-export async function buildActionPlan(candidate: OpportunityCandidate): Promise<ActionPlan> {
+export async function buildActionPlan(
+  candidate: OpportunityCandidate,
+  options?: { flashLoanToken?: any; flashLoanProvider?: FlashLoanProvider }
+): Promise<ActionPlan> {
   throw new Error('Classic Incentive not implemented');
 }
