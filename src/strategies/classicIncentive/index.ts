461a5ad4f0661c8e472374d05a91c391c173844d
Fauxfrenzy82 <rkmwogeza@gmail.com>
2026-08-31 17:53:13 +0300

    Update index.ts

--- src/strategies/classicIncentive/index.ts
+++ src/strategies/classicIncentive/index.ts
@@ -1,2 +1,12 @@
+// src/strategies/classicIncentive/index.ts
+
 export { discoverClassicIncentive } from './discover';
 export { buildActionPlan } from './buildActionPlan';
+export { 
+  ProtocolConfig, 
+  RewardType, 
+  HARVESTABLE_PROTOCOLS,
+  getHarvestableProtocols,
+  isHarvestable,
+  isHarvestLikeFunction,
+} from './protocolRegistry';
